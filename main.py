c1d7fd115961918d4fbf81265d3fad353e153f8f
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-01-22 22:42:13 +0300

    Fix main logic

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
-import os
 import psutil
 import resources_utilities
 import argparse
 import time
 from datetime import datetime
 import save_results
+import execute_application
 
 
-def main(cpu_interval, stat_interval):
-    process = psutil.Process()
-    pid = os.getpid()
+def main(cpu_interval, stat_interval, app_path):
+    app_pid = execute_application.execute_app_macos(app_path)
+    time.sleep(1)
+    process = psutil.Process(app_pid)
     measures = []
     stats = {}
-    launch_time = datetime.now()
+    launch_time = datetime.now().strftime("%d.%m.%Y %H-%M-%S")
     while True:
         timestamp = datetime.now()
-        cpu_usage = resources_utilities.get_cpu_usage_info(
-            process, cpu_interval
+        measure = resources_utilities.get_all_utilization(
+            process, cpu_interval, timestamp, app_pid
         )
-        memory_usage = resources_utilities.get_memory_usage_info(process)
-        file_descriptors = resources_utilities.get_file_descriptors_info(process)
-        measure = {
-            'Date': timestamp.strftime("%d/%m/%Y, %H:%M:%S"),
-            'Process ID': pid,
-            'CPU Utilization': f'{cpu_usage}%',
-            'Resident Set Size': memory_usage.rss,
-            'Virtual Memory Size': memory_usage.vms,
-            'File Descriptors': file_descriptors
-        }
         measures.append(measure)
         stats["resource_measures"] = measures
         save_results.save_statistics_to_file(stats, launch_time)
@@ -42,5 +33,13 @@ if __name__ == "__main__":
         'stats_frequency', type=int, help='''
         frequency in seconds for new measure to appear'''
     )
+    parser.add_argument(
+        'app_path', type=str, help='path to application to monitor'
+    )
     args = parser.parse_args()
-    main(args.cpu_interval, args.stats_frequency)
+    try:
+        main(args.cpu_interval, args.stats_frequency, args.app_path)
+    except psutil.NoSuchProcess:
+        print('Script execution stopped due to process stop')
+    except psutil.AccessDenied:
+        print('Give access to folder/file')
