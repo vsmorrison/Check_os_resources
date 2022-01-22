a41753ce2d99a55a063c7ba688092313e03b6be3
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-01-22 22:42:57 +0300

    Add total utilization function

--- resources_utilities.py
+++ resources_utilities.py
@@ -11,3 +11,18 @@ def get_memory_usage_info(process):
 def get_file_descriptors_info(process):
     file_descriptors = process.num_fds()
     return file_descriptors
+
+
+def get_all_utilization(process, cpu_interval, timestamp, pid):
+    cpu_usage = get_cpu_usage_info(process, cpu_interval)
+    memory_usage = get_memory_usage_info(process)
+    file_descriptors = get_file_descriptors_info(process)
+    measure = {
+        'Date': timestamp.strftime("%d/%m/%Y, %H:%M:%S"),
+        'Process ID': pid,
+        'CPU Utilization': f'{cpu_usage}%',
+        'Resident Set Size': memory_usage.rss,
+        'Virtual Memory Size': memory_usage.vms,
+        'File Descriptors': file_descriptors
+    }
+    return measure
