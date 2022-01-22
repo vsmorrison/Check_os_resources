5eddd17e06be1b4fc37c57dd08a6647cba9e4f53
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-01-22 22:41:23 +0300

    Add filename format

--- save_results.py
+++ save_results.py
@@ -2,6 +2,6 @@ import json
 
 
 def save_statistics_to_file(stats, time):
-    filepath = f'Process utilization{time.strftime("[%d-%m-%Y %H:%M:%S]")}.json'
+    filepath = f'Process report [{time}].json'
     with open(filepath, 'w', encoding='utf-8') as file:
         json.dump(stats, file)
