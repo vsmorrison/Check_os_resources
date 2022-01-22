d89121269685eec03230335e200a622daed1087e
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-01-22 22:03:35 +0300

    Added flag for open

--- execute_application.py
+++ execute_application.py
@@ -2,6 +2,6 @@ import subprocess
 
 
 def execute_app_macos(app_path):
-    parent = subprocess.Popen(['open', app_path])
+    parent = subprocess.Popen(['open', '-a', app_path])
     application_pid = parent.pid + 1
     return application_pid
